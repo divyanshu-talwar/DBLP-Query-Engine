57ee6321752f7aa42dd9a5ee21a27a243c922b30
Mridul Gupta <mridulg@users.noreply.github.com>
2016-11-13 16:18:45 +0530

    added move to eclipse project script. Irrelevant.

---
+++ workspace/src/eclipse.sh
@@ -0,0 +1,13 @@
+cp Data.java Database.java Parser.java Query1Handler.java Query2Handler.java Query3Handler.java ResultPanel.java mainClass.java myFrame.java myPanel.java myQuery1Panel.java myQuery2Panel.java myQuery3Panel.java ~/Documents/workspace/DBLP\ Query\ Engine/src/
+# cp Database.java /Users/mridul/Documents/workspace/DBLP Query Engine/src/
+# cp Parser.java /Users/mridul/Documents/workspace/DBLP Query Engine/src/
+# cp Query1Handler.java /Users/mridul/Documents/workspace/DBLP Query Engine/src/
+# cp Query2Handler.java /Users/mridul/Documents/workspace/DBLP Query Engine/src/
+# cp Query3Handler.java /Users/mridul/Documents/workspace/DBLP Query Engine/src/
+# cp ResultPanel.java /Users/mridul/Documents/workspace/DBLP Query Engine/src/
+# cp mainClass.java /Users/mridul/Documents/workspace/DBLP Query Engine/src/
+# cp myFrame.java /Users/mridul/Documents/workspace/DBLP Query Engine/src/
+# cp myPanel.java /Users/mridul/Documents/workspace/DBLP Query Engine/src/
+# cp myQuery1Panel.java /Users/mridul/Documents/workspace/DBLP Query Engine/src/
+# cp myQuery2Panel.java /Users/mridul/Documents/workspace/DBLP Query Engine/src/
+# cp myQuery3Panel.java /Users/mridul/Documents/workspace/DBLP Query Engine/src/
