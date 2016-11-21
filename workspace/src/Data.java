f9502e320309d491081a2fa1072c5188fc8c614b
Mridul Gupta <mridulg@users.noreply.github.com>
2016-11-21 20:12:24 +0530

    code formatiing and doxygen (1)

--- workspace/src/Data.java
+++ workspace/src/Data.java
@@ -1,130 +1,116 @@
 import java.util.ArrayList;
 
 public class Data implements Comparable {
-    private String title;
-    private int year;
-    private String volume;
-    private String pages;
-    private String journal_booktitle;
-    private ArrayList<String> authors;
-    private ArrayList<String> url;
+	private String title;
+	private int year;
+	private String volume;
+	private String pages;
+	private String journal_booktitle;
+	private ArrayList<String> authors;
+	private ArrayList<String> url;
 
-    public Data()
-    {
-        title="NA";
-        volume="NA";
-        pages="NA";
-        year=0;
-        journal_booktitle="NA";
-        authors= new ArrayList<>();
-        url= new ArrayList<>();
-    }
+	public Data() {
+		title = "NA";
+		volume = "NA";
+		pages = "NA";
+		year = 0;
+		journal_booktitle = "NA";
+		authors = new ArrayList<>();
+		url = new ArrayList<>();
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public void setYear(int year) {
-        this.year = year;
-    }
+	public void setYear(int year) {
+		this.year = year;
+	}
 
-    public void setVolume(String volume) {
-        this.volume = volume;
-    }
+	public void setVolume(String volume) {
+		this.volume = volume;
+	}
 
-    public void setPages(String pages) {
-        this.pages = pages;
-    }
+	public void setPages(String pages) {
+		this.pages = pages;
+	}
 
-    public void setJournal_booktitle(String journal_booktitle) {
-        this.journal_booktitle = journal_booktitle;
-    }
+	public void setJournal_booktitle(String journal_booktitle) {
+		this.journal_booktitle = journal_booktitle;
+	}
 
+	public void addAuthor(String _author) {
+		authors.add(_author);
+	}
 
-    public void addAuthor(String _author) {
-        authors.add(_author);
-    }
+	public void addUrl(String _url) {
+		url.add(_url);
+	}
 
-    public void addUrl(String _url) {
-        url.add(_url);
-    }
-    
-    public boolean searchAuthor(String name){
-    	System.out.println("Size is "+authors.size());
-    	for(int i=0;i<authors.size();i++){
-    		if(authors.get(i).toLowerCase().equals(name.toLowerCase()))
-    			return true;
-    	}
-    	return false;
-    }
-    
-    public String getAuthor()
-    {
-        String temp=authors.get(0);
-        for(int i=1;i<authors.size();i++)
-        {
-            temp+=" ,"+authors.get(i);
-        }
-        return temp;
+	public boolean searchAuthor(String name) {
+		System.out.println("Size is " + authors.size());
+		for (int i = 0; i < authors.size(); i++) {
+			if (authors.get(i).toLowerCase().equals(name.toLowerCase()))
+				return true;
+		}
+		return false;
+	}
 
-    }
+	public String getAuthor() {
+		String temp = authors.get(0);
+		for (int i = 1; i < authors.size(); i++) {
+			temp += " ," + authors.get(i);
+		}
+		return temp;
 
-    public String getUrl()
-    {
-        try
-        {
-        	String temp=url.get(0);
-            if(temp == null)
-            {
-                return "NA";
-            }
-            for(int i=1;i<url.size();i++)
-            {
-                temp+=", "+url.get(i);
-            }
-            return temp;
-        }
-        catch (Exception e)
-        {
-            return "NA";
-        }
-    }
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getUrl() {
+		try {
+			String temp = url.get(0);
+			if (temp == null) {
+				return "NA";
+			}
+			for (int i = 1; i < url.size(); i++) {
+				temp += ", " + url.get(i);
+			}
+			return temp;
+		} catch (Exception e) {
+			return "NA";
+		}
+	}
 
-    public int getYear() {
-        return year;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public String getVolume() {
-        return volume;
-    }
+	public int getYear() {
+		return year;
+	}
 
-    public String getPages() {
-        return pages;
-    }
+	public String getVolume() {
+		return volume;
+	}
 
-    public String getJournal_booktitle() {
-        return journal_booktitle;
-    }
+	public String getPages() {
+		return pages;
+	}
 
-    public ArrayList<String> getRawAuthor()
-    {
-        return authors;
-    }
+	public String getJournal_booktitle() {
+		return journal_booktitle;
+	}
 
+	public ArrayList<String> getRawAuthor() {
+		return authors;
+	}
 
-    @Override
-    public int compareTo(Object o) {
-        int temp= ((Data) o).year;
-        return temp-this.year;
-    }
+	@Override
+	public int compareTo(Object o) {
+		int temp = ((Data) o).year;
+		return temp - this.year;
+	}
 
-    public String toString()
-    {
-        return authors+" "+title+" "+pages+" "+volume+" "+journal_booktitle+" "+year+" "+url;
-    }
+	public String toString() {
+		return authors + " " + title + " " + pages + " " + volume + " " + journal_booktitle + " " + year + " " + url;
+	}
 }
-
