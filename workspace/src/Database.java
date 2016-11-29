6852d600b3a917193600c0ae6bc109375dbc12f0
Mridul Gupta <mridulg@users.noreply.github.com>
2016-11-29 14:05:46 +0530

    Doxygen for Data files

--- workspace/src/Database.java
+++ workspace/src/Database.java
@@ -1,11 +1,11 @@
 /**
- *
+ * Stores the Content from the Parsed XML files to Arraylist<Data>
  * @author Mridul Gupta | Divyanshu Talwar
  */
 import java.util.ArrayList;
 
 public class Database {
-	public static ArrayList<Data> allData = new ArrayList<Data>();
-	public static ArrayList<Author> authors = new ArrayList<Author>();
+	public static ArrayList<Data> allData = new ArrayList<Data>(); /**< ArrayList of Data. */ 
+	public static ArrayList<Author> authors = new ArrayList<Author>(); /**< ArrayList of Authors. */ 
 	
 }
